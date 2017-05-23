fa759b62dd47d9f9d2ec45bb44796845a354de4e
gentaman <gentaman01@gmail.com>
2017-05-23 23:35:05 +0900

    fix Kullback-Leiber divergence

--- sparse_autoencoder.py
+++ sparse_autoencoder.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 
 
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
-H =100
+H =1000
 BATCH_SIZE = 100
 DROP_OUT_RATE = 0.5
 
@@ -22,26 +22,28 @@ x = tf.placeholder(tf.float32, [None, 784])
 # Variable: W, b1
 #W = weight_variable((784, H))
 #b1 = bias_variable([H])
-W = tf.Variable(tf.random_normal([784, H], stddev=0.1), name="W")
+W = tf.Variable(tf.random_normal([784, H], stddev=0.01), name="W")
 b1 = tf.Variable(tf.zeros([H]), name="b1")
 
 
 # Hidden Layer: h
 # softsign(x) = x / (abs(x)+1); https://www.google.co.jp/search?q=x+%2F+(abs(x)%2B1)
 h = tf.nn.softsign(tf.matmul(x, W) + b1)
+#min_i = tf.argmin(h)
+#h1 = h[min_i[0]]
 h = tf.nn.relu(h)
 keep_prob = tf.placeholder("float")
 h_drop = tf.nn.dropout(h, keep_prob)
 
 # Variable: b2
-W2 = tf.transpose(W, name="W2")  # 転置
+W2 = tf.transpose(W, name="W2") 
 b2 = tf.Variable(tf.zeros([784]), name="b2")
 y = tf.nn.relu(tf.matmul(h, W2) + b2)
 
 # Define Loss Function
-p1 = tf.reduce_mean(h, 0, name='p1')
-p = 0.005
-KL = p*tf.log(p/p1) + (1-p)*tf.log((1-p)/(1-p1))
+p1 = tf.reduce_mean(h, axis=0, name='p1')+1e-5
+p = tf.ones(p1.shape)*0.005
+KL = p*tf.log(tf.abs(p/p1)) + (1-p)*tf.log(tf.abs((1-p)/(1-p1)))
 KL_reg = 0.3*tf.reduce_sum(KL, name="KL")
 loss =  KL_reg + tf.nn.l2_loss(y - x) / BATCH_SIZE
 
@@ -67,10 +69,10 @@ for step in range(4000):
     sess.run(train_step, feed_dict={x: batch_xs, keep_prob: (1-DROP_OUT_RATE)})
     # Collect Summary
     summary_op = tf.summary.merge_all()
-    summary_str = sess.run(summary_op, feed_dict={x: batch_xs, keep_prob: 1.})
+    summary_str = sess.run(summary_op, feed_dict={x: batch_xs, keep_prob: .5})
     summary_writer.add_summary(summary_str, step)
     # Print Progress
     if step % 100 == 0:
-        _data = sess.run([loss, KL_reg], feed_dict={x: batch_xs, keep_prob: 1.})
-        print("loss {0} \t  KL regulazation {1}".format(_data[0], _data[1]))
+        _data = sess.run([loss, KL_reg, KL], feed_dict={x: batch_xs, keep_prob: .5})
+        print("loss {0} \t  KL regulazation {1}\t \n".format(_data[0], _data[1]))
 
